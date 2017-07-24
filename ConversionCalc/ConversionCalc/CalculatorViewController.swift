e67bb6d0cdfd49e71b819bd0791e0ddca3422423
Carl Manganelli <carl@Carls-MacBook-Pro-3.local>
2017-07-24 12:02:04 -0500

    Built UI and replaced default ViewController

---
+++ ConversionCalc/ConversionCalc/CalculatorViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CalculatorViewController.swift
+//  ConversionCalc
+//
+//  Created by Carl Manganelli on 7/24/17.
+//  Copyright © 2017 Carl Manganelli. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
